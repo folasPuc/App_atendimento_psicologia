93e1efa280f2fa4993a5f7457c827b77db02a0ca
enderxdxd <henriquepcosta@hotmail.com>
2024-01-29 23:25:33 -0300

    penisspenis
    penispenis

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App 
+from kivy.lang import Builder 
+
+GUI= Builder.load_file('main.kv')
+
+
+class MainApp(App):
+    def build(self):
+        return GUI
+    
+
+
+MainApp().run()
