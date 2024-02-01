a713d1b899b528824e0145e6e249c0ce775c577d
folasPuc <rafael.rsato@gmail.com>
2024-02-01 00:37:32 -0300

    registrar_penis
    sadsadsadsad

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.label import Label
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
+from myfirebase import MyFirebase
+import requests
 
 import unicodedata
 # coding: utf-8
@@ -22,10 +24,15 @@ GUI= Builder.load_file('main.kv')
 
 class MainApp(App):
     def build(self):
+        self.myfirebase = MyFirebase()
         return GUI
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids["screen_manager"]
-        gerenciador_telas.current = id_tela    
+        gerenciador_telas.current = id_tela     
+    def carregar_infos_usuario(self):
+        requisicao = requests.get(f"https://app-psicologia-66b64-default-rtdb.firebaseio.com/{self.local_id}.json")
+        requisicao_dic = requisicao.json()
+        # email = requisicao_dic[]
 
 
 
