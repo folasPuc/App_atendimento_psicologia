f7e8f6de7c24ddecf286c7a69c4d457e5e9774b3
folasPuc <rafael.rsato@gmail.com>
2024-02-02 00:34:50 -0300

    login_reg_token
    wqeqweqweqwe

--- main.py
+++ main.py
@@ -26,13 +26,29 @@ class MainApp(App):
     def build(self):
         self.myfirebase = MyFirebase()
         return GUI
+    
+    def on_start(self):
+        self.carregar_infos_usuario()   
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids["screen_manager"]
-        gerenciador_telas.current = id_tela     
+        gerenciador_telas.current = id_tela  
     def carregar_infos_usuario(self):
-        requisicao = requests.get(f"https://app-psicologia-66b64-default-rtdb.firebaseio.com/{self.local_id}.json")
-        requisicao_dic = requisicao.json()
-        # email = requisicao_dic[]
+        try:
+            with open("refreshtoken.txt", "r") as arquivo:
+                refresh_token = arquivo.read()
+            local_id, id_token = self.myfirebase.trocar_token(refresh_token)
+            self.local_id = local_id
+            self.id_token = id_token
+            requisicao = requests.get(f"https://app-psicologia-66b64-default-rtdb.firebaseio.com/{self.local_id}.json")
+            requisicao_dic = requisicao.json()
+            email = requisicao_dic["email"]
+            telefone = requisicao_dic["telefone"]
+            email_user = self.root.ids.menu.ids.email_usuario.text = "[b]Email:[/b] {}".format(email)
+            tel_user = self.root.ids.menu.ids.telefone_usuario.text = "[b]Telefone:[/b] {}".format(telefone)
+            self.mudar_tela("menu")
+        except:
+            pass
+        
 
 
 
